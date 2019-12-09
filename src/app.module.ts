4b11059fb45ada3c191aa04ae9d36b36a3de23c0
Felipe Godoy <felipe.godoy@ceinf.cl>
2019-12-09 00:55:29 -0300

    add nuevo modulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { MedicionModule } from './medicion/medicion.module';
 
 @Module({
   imports: [
     AuthModule,
-    UserModule
+    UserModule,
+    MedicionModule
   ],
   providers: [],
+  controllers: [],
 })
 export class AppModule {}
