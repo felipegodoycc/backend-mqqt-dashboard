7aa192a6e2f2cf45faf7996160578fcaacd4378f
Felipe Godoy <felipe.godoy@ceinf.cl>
2019-12-09 00:55:41 -0300

    fix module medicoin

---
+++ src/medicion/medicion.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
+import { MedicionController } from './medicion.controller';
+import { MedicionService } from './medicion.service';
+import { medicionProviders } from './medicion.providers';
+
+@Module({
+    imports: [
+        DatabaseModule
+    ],
+    controllers: [MedicionController],
+    providers: [MedicionService, ...medicionProviders],
+    exports: [MedicionService]
+})
+export class MedicionModule {}
