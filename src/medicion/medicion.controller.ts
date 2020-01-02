49afb50c382845f4a38f6ed3916acc3c8495e54d
Felipe Godoy <felipegodoy@workmate.cl>
2020-01-01 22:31:57 -0300

    fix

--- src/medicion/medicion.controller.ts
+++ src/medicion/medicion.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Res, Param, HttpStatus, Query } from '@nestjs/common';
 import { MedicionService } from './medicion.service';
 import { Response } from 'express';
+import { getHeapStatistics } from 'v8';
 
 @Controller('medicion')
 export class MedicionController {
@@ -25,4 +26,22 @@ export class MedicionController {
             })
         })
     }
+
+    @Get('porHoras/desde/:desde/hasta/:hasta/')
+    async GetMedicionesPorHora(@Res() res:Response, @Param('desde') desde, @Param('hasta') hasta, @Query('topic') topic){
+        this.medicionService.getPorHora(desde,hasta,topic)
+        .then( data => {
+            return res.status(HttpStatus.OK).json({
+                ok: true,
+                items: data,
+            })
+        })
+        .catch( err =>{ 
+            console.log("Error ", err)
+            return res.status(HttpStatus.BAD_REQUEST).json({
+                err
+            })
+        })
+    }
+
 }
--- src/medicion/medicion.service.ts
+++ src/medicion/medicion.service.ts
@@ -11,8 +11,23 @@ export class MedicionService {
         const query = { date: { $gte: desde, $lte: hasta }};
         let all = await this.medicionModel.findAndFilter(query, options, []);
         let total = await this.medicionModel.findAndFilter(query,options,[], false)
-        total = total.length;    
+        total = total.length;
         return { all, total }
+    }
+
+    async getPorHora(desde:Date, hasta:Date, topic: string){
+        let all = await this.medicionModel.aggregate([
+            { $match: {
+                        date: {
+                                $gte: new Date(desde),
+                                $lte: new Date(hasta) },
+                        topic }
+                    },
+            { $project: { value: 1, topic: 1 , hora: { $hour: '$date'} }},
+            { $group: { _id: '$hora', value: { $avg: '$value'} }},
+           ])
+
+        return all;
     }
 
 }
