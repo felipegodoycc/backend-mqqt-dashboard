336e4c36e6ce29034d7fbd879e8564ed2e28be81
Felipe Godoy <felipegodoy@workmate.cl>
2020-01-21 10:27:26 -0300

    import passport

--- src/ewelink/ewelink.module.ts
+++ src/ewelink/ewelink.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EwelinkService } from './ewelink.service';
 import { EwelinkController } from './ewelink.controller';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt', session: false})
+  ],
   providers: [EwelinkService],
   controllers: [EwelinkController]
 })
--- src/medicion/medicion.module.ts
+++ src/medicion/medicion.module.ts
@@ -3,10 +3,12 @@ import { DatabaseModule } from '../database/database.module';
 import { MedicionController } from './medicion.controller';
 import { MedicionService } from './medicion.service';
 import { medicionProviders } from './medicion.providers';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
     imports: [
-        DatabaseModule
+        DatabaseModule,
+        PassportModule.register({ defaultStrategy: 'jwt', session: false})
     ],
     controllers: [MedicionController],
     providers: [MedicionService, ...medicionProviders],
